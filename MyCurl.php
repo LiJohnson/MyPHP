0a06005c22dc5d25b5265e5086ec9cdfd586b7e5
LiJohnson <598420668@163.com>
2013-10-19 11:54:15 +0000

    git-svn-id: https://svn.sinaapp.com/theotherdoor/2/class@585 8ae2ba1d-993d-4c9a-aded-4246d96bdaa4

--- MyCurl.php
+++ MyCurl.php
@@ -1,10 +1,12 @@
 <?php 
 
-class MyCurl{ 
+class MyCurl{
+	private $headers;
 	public $curlHandle ;
 	function __construct( $url = null ) {
 		 $this->curlHandle = curl_init($url);
 		 $this->setOption(CURLOPT_RETURNTRANSFER, 1);
+		 $this->headers = array();
 	}
 	function __destruct(){
 		//curl_close($this->curlHandle);
@@ -21,20 +23,30 @@ class MyCurl{
 		if( $url != null){
 			$this->setOption(CURLOPT_URL,$url);
 		}
+		$headerArr = array();
+		foreach ($this->headers as $key => $value) {
+			$headerArr[] = $key . ":".$value;
+		}
+		$this->setOption(CURLOPT_HTTPHEADER , $headerArr);
 		return curl_exec($this->curlHandle);
 	}
 	
-	function getInfo(){
-		return curl_getinfo($this->curlHandle);
+	function getInfo($key = false){
+		if( $key == false )return curl_getinfo($this->curlHandle);
+		return curl_getinfo($this->curlHandle , $key);
+
 	}
 	function setOption( $key , $value ){
 		curl_setopt( $this->curlHandle,  $key, $value );
 	}
 	
-	function setCookieOn($value = "tmp_cookies")
-	{
+	function setCookieOn($value = "tmp_cookies"){
 		curl_setopt( $this->curlHandle,  CURLOPT_COOKIEJAR, $value );
 		curl_setopt( $this->curlHandle,  CURLOPT_COOKIEFILE, $value );
+	}
+
+	function setHeader($key , $value){
+		$this->headers[$key] = $value;
 	}
 }
 
