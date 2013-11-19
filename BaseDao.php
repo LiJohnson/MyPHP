7664ee63a9ce1f35a4c9e2d086e2d0d1e836e893
LiJohnson <598420668@163.com>
2013-11-19 10:00:36 +0000

    git-svn-id: https://svn.sinaapp.com/theotherdoor/2/class@595 8ae2ba1d-993d-4c9a-aded-4246d96bdaa4

--- BaseDao.php
+++ BaseDao.php
@@ -90,8 +90,9 @@ class BaseDao extends MySql {
 		if( $page && $page['pageSize'] > 0 ){
 			$page['page'] = $page['page'] ? $page['page'] : 1;
 			$totalRecord = $this->getVar( preg_replace('/^select[\s\*]+from/', 'select count(1) from', $sql)   );
+			$page['totalRecord'] = $totalRecord
 			$page['total'] = floor( $totalRecord / $page['pageSize'] ) + (  $totalRecord % $page['pageSize'] ? 1 : 0);
-			$page['total'] = $page['total'] ? 1 :$page['total'] ;
+			$page['total'] = !$page['total'] ? 1 :$page['total'] ;
 			$page['page'] = $page['page'] < $page['total'] ?  $page['page'] : $page['total'] ;
 			$sql .= ' limit ' .($page['page']-1)*$page['pageSize'] . ',' .$page['pageSize'];
 			
